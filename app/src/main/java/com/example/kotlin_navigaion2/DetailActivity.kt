6f8362da58d1c0c45aa387d3fcbadf39f6f3a7ab
kimsungeun <mirxx@naver.com>
2021-04-09 18:41:14 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kotlin_navigaion2/DetailActivity.kt
@@ -0,0 +1,39 @@
+package com.example.kotlin_navigaion2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.view.View.inflate
+import android.view.accessibility.AccessibilityNodeInfo
+import androidx.core.content.res.ColorStateListInflaterCompat.inflate
+import androidx.core.content.res.ComplexColorCompat.inflate
+import androidx.core.graphics.drawable.DrawableCompat.inflate
+import com.example.kotlin_navigaion2.databinding.ActivityDetailBinding
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+
+class DetailActivity : AppCompatActivity() {
+    companion object {
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val letterId = intent?.extras?.getString(LETTER).toString()
+
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = WordAdapter(letterId, this)
+
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        )
+
+        title = getString(R.string.detail_prefix) + " " + letterId
+    }
+}
